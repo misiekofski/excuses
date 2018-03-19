7c0fdb64ed5ec146424612e0e453762d74a30f3d
Michal Dobrzycki <dobrzycki@gmail.com>
2018-03-19 21:10:32 +0100

    Added some templates, views and links

--- excuses/urls.py
+++ excuses/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /excuses/
     path('', views.index, name='index'),
+    # ex: /excuses/2
+    path('<int:excuse_id>/', views.excuse, name='excuse')
 ]
--- excuses/views.py
+++ excuses/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from excuses.models import RootCause
+from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Stop byte was received, and app crashed")
+    latest_excuses_list = RootCause.objects.all()[:10]
+    template = loader.get_template('index.html')
+    context = {
+        'latest_excuses_list': latest_excuses_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def excuse(request, excuse_id):
+    try:
+        excuse_detail = RootCause.objects.get(pk=excuse_id)
+    except:
+        raise Http404("This content is not available in your country xD")
+    return render(request, 'detail.html', {'excuse_detail': excuse_detail})
