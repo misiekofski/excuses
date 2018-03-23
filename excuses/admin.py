b196647943fa05fbe0c135c2a4ec0d1043e0c35c
Michal Dobrzycki <dobrzycki@gmail.com>
2018-03-23 09:14:09 +0100

    Added ActionPlan model to projeect

--- excuses/admin.py
+++ excuses/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import RootCause
+from .models import RootCause, ActionPlan
 
 admin.site.register(RootCause)
+admin.site.register(ActionPlan)
--- excuses/models.py
+++ excuses/models.py
@@ -6,3 +6,10 @@ class RootCause(models.Model):
     cause_text = models.CharField(max_length=500)
     def __str__(self):
         return self.cause_text
+
+class ActionPlan(models.Model):
+    rootcause = models.ForeignKey(RootCause, related_name='actionplan',on_delete=models.CASCADE)
+    action_text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.action_text
